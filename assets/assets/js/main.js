ecebd90fccfe1be095ccf8b53a799b43dbe1df81
EthanHendricks <ethanmathendrix@gmail.com>
2026-02-10 16:38:36 +0200

    updatae

--- assets/assets/js/main.js
+++ assets/assets/js/main.js
@@ -1,4 +1,62 @@
+const canvas = document.createElement('canvas');
+canvas.id = 'bokeh-canvas';
+document.body.appendChild(canvas);
+const ctx = canvas.getContext('2d');
 
+let particles = [];
+const particleCount = 40;
+
+function initCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+class Particle {
+    constructor() {
+        this.reset();
+    }
+    reset() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 15 + 5;
+        this.speedY = Math.random() * 0.5 + 0.2;
+        this.opacity = Math.random() * 0.3;
+        // Brand Colors
+        const colors = ['#a24cc7', '#e64e9f', '#ec740b'];
+        this.color = colors[Math.floor(Math.random() * colors.length)];
+    }
+    update() {
+        this.y -= this.speedY;
+        if (this.y < -10) this.reset();
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.globalAlpha = this.opacity;
+        ctx.fill();
+    }
+}
+
+function createParticles() {
+    for (let i = 0; i < particleCount; i++) {
+        particles.push(new Particle());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(p => {
+        p.update();
+        p.draw();
+    });
+    requestAnimationFrame(animate);
+}
+
+window.addEventListener('resize', initCanvas);
+initCanvas();
+createParticles();
+animate();
 (function() {
   "use strict";
 
